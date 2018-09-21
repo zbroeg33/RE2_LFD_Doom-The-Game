236f0aa93a59032a8c85f0f7bbdb94b8f2526a7f
Zacharybroeg <Zacharybroeg@u.boisestate.edu>
2018-09-20 21:24:46 -0600

    spawning pickups work

--- Fortnite 3/Assets/Enemy Assets/EnemySpawner.cs
+++ Fortnite 3/Assets/Enemy Assets/EnemySpawner.cs
@@ -4,7 +4,10 @@ using UnityEngine.Networking;
 public class EnemySpawner : NetworkBehaviour {
 
 	public GameObject enemyPrefab;
+    public GameObject bulletPrefab;
     public int numberOfEnemies;
+
+    public int numberOfBullets;
 
     public override void OnStartServer()
     {
@@ -22,6 +25,22 @@ public class EnemySpawner : NetworkBehaviour {
 
             var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
             NetworkServer.Spawn(enemy);
+        }
+
+        for (int i=0; i < numberOfBullets; i++)
+        {
+            var spawnPosition = new Vector3(
+                Random.Range(transform.position.x - 10.0f, transform.position.x + 10.0f),
+                0.0f,
+                Random.Range(transform.position.z -10.0f, transform.position.z + 10.0f));
+
+            var spawnRotation = Quaternion.Euler( 
+                0.0f, 
+                Random.Range(0,180), 
+                0.0f);
+
+            var bullet = (GameObject)Instantiate(bulletPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(bullet);
         }
     }
 }
